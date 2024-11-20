3771d5ed10c30e00831fc1ca767b53502f55c68c
Lulhum <lulhum@lulhum.org>
2024-11-20 17:52:07 +0100

    wip

---
+++ src/test.py
@@ -0,0 +1,13 @@
+# Load model directly
+from transformers import pipeline
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+
+tokenizer = AutoTokenizer.from_pretrained("tblard/tf-allocine")
+model = TFAutoModelForSequenceClassification.from_pretrained("tblard/tf-allocine")
+
+nlp = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+
+print(nlp("Alad'2 est clairement le meilleur film de l'année 2018.")) # POSITIVE
+print(nlp("Juste whoaaahouuu !")) # POSITIVE
+print(nlp("NUL...A...CHIER ! FIN DE TRANSMISSION.")) # NEGATIVE
+print(nlp("Je m'attendais à mieux de la part de Franck Dubosc !")) # NEGATIVE
